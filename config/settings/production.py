c8603e2ac5a331b8e17f9c5f7ab450fb3d5cd69a
techsolutionss <aulematins@gmail.com>
2025-01-14 13:38:24 +0100

    changes

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,9 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["ani.com"])
+#ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["ani.com"])
+ALLOWED_HOSTS = ["americanationalinsurance.com", "www.americanationalinsurance.com"]
+
 
 
 # DATABASES
