a3ca71c0050274d63c2f420c7813c0add679657f
techsolutionss <aulematins@gmail.com>
2025-01-24 13:38:35 +0100

    changes

--- ani_apps/anipages/urls.py
+++ ani_apps/anipages/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.utils.translation import gettext_lazy as _
-from .views import HomeTemplateView
+from .views import HomeTemplateView, Insurance, contacts, information_form
 
 
 app_name = "anipages"
  
 urlpatterns = [
     path("", view=HomeTemplateView.as_view(), name="home"),
+    path("contacts/", view=contacts, name="contacts"),
+    path("insurance/", view=Insurance.as_view(), name="insurance"),
+    path("get-started/", view=information_form, name="get_started")
+
 ]
--- ani_apps/anipages/views.py
+++ ani_apps/anipages/views.py
@@ -4,3 +4,14 @@ from django.views.generic.base import TemplateView
 
 class HomeTemplateView(TemplateView):
     template_name = "anipages/home.html"
+
+
+def contacts(request):
+    return render(request, "anipages/contacts.html")
+
+
+class Insurance(TemplateView):
+    template_name = "anipages/insurance.html"
+
+def information_form(request):
+    return render(request, "anipages/information_form.html")
